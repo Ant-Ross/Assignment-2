2eeb147dd2f7a0db8c10b8c1c6de264871a8111f
Ant-Ross <Luis.RossPerez@postgrad.manchester.ac.uk>
2019-01-04 17:25:40 +0000

    040118

--- Bomb_model_v1.py
+++ Bomb_model_v1.py
@@ -5,7 +5,6 @@ Created on Thu Jan  3 13:45:31 2019
 @author: mcyitlr4
 """
 
-import random
 import matplotlib.pyplot
 import matplotlib.animation
 import csv
@@ -26,25 +25,51 @@ f.close()
 # Locating the building's coordinates.
 row_counter = 0
 for row in environment:
-    row_counter += 1
     for value in row:
         if value != 0:
             x = row.index(value)
             y = row_counter
-            print(x)
-            print(row_counter)
+            #print("Weapon located at coordinates","x =", x, "y =", y)
+            print(environment[y][x])
+    row_counter += 1
 
 # Parameters          
-start_height = 75
-num_of_particles = 50
+start_height = 76
+num_of_particles = 5000
 particles = []
 
 # Creating particles
 for i in range(num_of_particles):
     particles.append(agent_particle.Particle(x, y, start_height))
 
-particles[0].y
-matplotlib.pyplot.imshow(environment) 
-matplotlib.pyplot.scatter(50,160, marker="*", color='yellow')           
+# Spreading the particles. The algorithm will move each particle, one by one, 
+# until it reaches the ground.
+    
+for j in range(len(particles)):
+    while particles[j].height != 0:
+        particles[j].direction()
+        particles[j].fall()
+        #print("particle", j, particles[j].height, "m")
 
-random.random()
+    
+# Plotting particles
+for z in range(len(particles)):
+    environment[particles[z].y][particles[z].x] += 5
+
+    
+max(environment)
+
+
+#for k in range(len(particles)):
+#   matplotlib.pyplot.scatter(particles[k].x,particles[k].y, color='green')
+#matplotlib.pyplot.scatter(x,y, color='red')
+matplotlib.pyplot.imshow(environment)            
+#matplotlib.pyplot.ylim(0, 300)
+#matplotlib.pyplot.xlim(0, 300)
+
+
+#environment[particles[0].y][particles[0].x] = 150
+#print((particles[0].y),(particles[0].x))  
+
+#print(environment[particles[0].y][particles[0].x])
+#print(environment[159][99])
--- agent_particle.py
+++ agent_particle.py
@@ -5,7 +5,7 @@ Created on Thu Jan  3 13:59:47 2019
 @author: mcyitlr4
 """
 
-import random
+import random   
 
 class Particle:
     def __init__(self, x, y, start_height):
@@ -14,8 +14,29 @@ class Particle:
         self.height = start_height
     
     def direction(self) :
-        if random.random() <= 0.75:
+        rndm = random.random()
+        #print(rndm)
+        if rndm <= 0.75:
             self.x = (self.x + 1) % 300 # change the boundary rule
-        elif 0.75 < random.random() <= 0.85: # You are generating new random numbers. We need to use the same one.
+        elif 0.75 < rndm <= 0.85: 
             self.x = (self.x - 1) % 300 
+        elif 0.85 < rndm <= 0.95:
+            self.y = (self.y + 1) % 300
+        elif 0.95 < rndm <= 1:
+            self.y = (self.y - 1) % 300 
        
+    def fall(self):
+        if self.height > 75:
+            rndm = random.random()
+            #print(rndm)
+            if rndm <= 0.20:
+                self.height += 1
+            elif 0.20 < rndm <= 0.30:
+                self.height = self.height
+            elif 0.30 < rndm <= 1:
+                self.height -= 1
+        else:
+            self.height -= 1
+            
+          
+   
